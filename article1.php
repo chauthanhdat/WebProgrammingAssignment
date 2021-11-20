0fdad0907cb5b33af6e5a2e31fcbcb7fc37a99af
CHAUTHANHDAT\chaut <dat@wolffungame.com>
2021-11-21 01:12:04 +0700

    update header and footer

---
+++ article1.php
@@ -0,0 +1,87 @@
+<?php
+include './inc/header.php'
+?>
+
+<main class="">
+    <section class="top-inner">
+        <div class="container">
+            <div class="row">
+                <div class="offset-lg-1 col-lg-10">
+                    <h1 class="title pb-4 mb-5">Tin tức</h1>
+                    <h2 class="sub-title">Fitfood VN hợp tác cùng H&amp;H Nutrition ra mắt &quot;Gói tư vấn dinh
+                        dưỡng chuyên sâu&quot;</h2>
+                    <div class="entry-meta mb-4">20/11/2020 By <a href="/">Fitfood</a></div>
+                    <div class="entry-content mb-4">
+                        <p>Cơ thể mỗi người khỏe mạnh l&agrave; sự c&acirc;n bằng. Trong qu&aacute; tr&igrave;nh
+                            sống v&agrave; sinh hoạt v&ocirc; t&igrave;nh hay cố &yacute; ch&uacute;ng ta đ&atilde;
+                            l&agrave;m mất sự c&acirc;n bằng đ&oacute;, sự mất c&acirc;n bằng k&eacute;o d&agrave;i
+                            sẽ g&acirc;y ra c&aacute;c rối loạn v&agrave; dẫn đến bệnh tật. Cung cấp dinh dưỡng hợp
+                            l&yacute; v&agrave; kịp thời sẽ th&uacute;c đẩy cơ thể ch&uacute;ng ta lấy lại sự
+                            c&acirc;n bằng nhanh hơn điều n&agrave;y sẽ gi&uacute;p hạn chế phải sử dụng c&aacute;c
+                            loại thuốc điều trị. Nhưng để lựa chọn c&aacute;c sản phẩm dinh dưỡng ch&iacute;nh
+                            thống, khoa học kh&ocirc;ng phải l&agrave; điều dễ d&agrave;ng m&agrave; phải cần đến
+                            những chuy&ecirc;n gia dinh dưỡng tư vấn v&agrave; lựa chọn nh&atilde;n hiệu uy
+                            t&iacute;n nhất.</p>
+
+                        <p><strong>H&amp;H Nutrition</strong>&nbsp;l&agrave; đơn vị chuy&ecirc;n cung cấp c&aacute;c
+                            sản phẩm dinh dưỡng, dịch vụ tư vấn, thiết kế thực đơn,&hellip; dựa tr&ecirc;n những
+                            thương hiệu nổi tiếng tr&ecirc;n thế giới đ&atilde; c&oacute; bằng chứng khoa học để
+                            chăm s&oacute;c, điều trị cho&nbsp;kh&aacute;ch h&agrave;ng.<strong>&nbsp;H&amp;H
+                                Nutrition</strong>&nbsp;sẽ cung cấp giải ph&aacute;p cho kh&aacute;ch h&agrave;ng
+                            th&ocirc;ng qua hệ thống Cửa h&agrave;ng dinh dưỡng, đội ngũ Chuy&ecirc;n gia v&agrave;
+                            ứng dụng c&ocirc;ng nghệ để mang giải ph&aacute;p dinh dưỡng tuyệt vời gi&uacute;p cho
+                            qu&yacute; kh&aacute;ch cải thiện sức khỏe to&agrave;n diện.</p>
+
+                        <p>Hiểu được tầm quan trọng của sức khỏe, dinh dưỡng đối với qu&yacute; kh&aacute;ch
+                            h&agrave;ng, Fitfood VN kết hợp c&ugrave;ng H&amp;H Nutrition ra mắt &quot;G&oacute;i tư
+                            vấn dinh dưỡng chuy&ecirc;n s&acirc;u&quot;.&nbsp;</p>
+
+                        <p><img class="img-fluid" alt="" src="images/product/article/FitfoodxHH.jpg" /></p>
+
+                        <h2><strong>G&oacute;i tư vấn dinh dưỡng chuy&ecirc;n s&acirc;u gồm c&aacute;c dịch vụ
+                                g&igrave;?</strong></h2>
+
+                        <p>G&oacute;i Tư Vấn Dinh Dưỡng Chuy&ecirc;n S&acirc;u của H&amp;H bao gồm:</p>
+
+                        <p>- Kh&aacute;m &amp;&nbsp;đo c&aacute;c chỉ số dinh dưỡng của cơ thể. BMI, mỡ cơ thể,
+                            huyết &aacute;p, tim mạch.</p>
+
+                        <p>- Đội ngũ Tư Vấn của H&amp;H sẽ chẩn đo&aacute;n, tư vấn chế độ dinh dưỡng &amp;
+                            c&aacute;c giải ph&aacute;p&nbsp;ph&ugrave; hợp t&ugrave;y thuộc v&agrave;o nhu cầu của
+                            bạn như giảm c&acirc;n, tăng cơ.</p>
+
+                        <p>- C&aacute;c khuyến nghị về chế độ dinh dưỡng, khẩu phần ăn, hướng dẫn vận động hoặc bổ
+                            sung c&aacute;c chất dinh dưỡng phụ trợ cần thiết kh&aacute;c.</p>
+
+                        <h2><strong>Đội ngũ b&aacute;c sĩ của H&amp;H Nutrition</strong></h2>
+
+                        <p>- Ths Đặng Ngọc H&ugrave;ng: Founder H&amp;H Nutrition. Chứng chỉ dinh dưỡng định hướng
+                            chuy&ecirc;n khoa dinh dưỡng l&acirc;m s&agrave;ng - ĐH Y Dược TPHCM</p>
+
+                        <p>- Ths B&ugrave;i Đ&igrave;nh Ho&agrave;n: Giảng vi&ecirc;n ĐH Y Dược, chuy&ecirc;n khoa
+                            dinh dưỡng. Chứng chỉ dinh dưỡng tiết chế l&acirc;m s&agrave;ng n&acirc;ng cao ĐH Y khoa
+                            Phạm Ngọc Thạch</p>
+
+                        <p>- Ths L&ecirc; Thị Thu Huyền: Chuy&ecirc;n khoa Y học cỗ truyền, chứng chỉ định hướng
+                            chuy&ecirc;n khoa da liễu</p>
+
+                        <p>Fitfood sẽ giao đến bạn Voucher G&oacute;i Tư Vấn Dinh Dưỡng khi bạn mua k&egrave;m phần
+                            ăn. </p>
+
+                        <p>Vui l&ograve;ng li&ecirc;n hệ &amp;&nbsp;đặt lịch trực tiếp với trung t&acirc;m H&amp;H
+                            để được tư vấn:</p>
+
+                        <p><strong>Đia chỉ:&nbsp;294/2 đường 3/2, Phường 12, Quận 10, TPHCM<br />
+                                Hotline: 088 8844 733<br />
+                                Website: https://dinhduongtoiuu.com</strong></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+</main>
+
+<?php
+include './inc/footer.php'
+?>
---
+++ inc/footer.php
@@ -0,0 +1,59 @@
+<footer class="footer-main bg-dark">
+    <div class="container-fluid ">
+        <a href="index.html" class="mb-4 d-block">
+            <img src="images/product/logo-fitfood.png" alt="" border="0" />
+        </a>
+        <div class="widget-footer mb-4">
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <h4>Công ty TNHH Fitfood</h4>
+                    <p>
+                        <strong>Địa chỉ</strong> 33 Đường 14, KDC Bình Hưng, Ấp 2, Huyện Bình Chánh, TPHCM<br />
+                        <strong>Điện thoại</strong> (+84) 932 788 120 [hotline] - (+84) 938 074 120 [sms]<br />
+                        <strong>Email</strong> info@fitfood.vn. For business inquiries:
+                        business@fitfood.vn<br />
+                        <strong>MST</strong> 0313272749 do Sở kế hoạch và đầu tư TPHCM cấp ngày 26/05/2015
+                    </p>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <h4>Điều khoản chung</h4>
+                    <ul>
+                        <li><a href="#">Chính Sách Quy Định Chung</a></li>
+                        <li><a href="#">Quy Định Hình Thức Thanh Toán</a></li>
+                        <li><a href="#">Chính Sách Vận Chuyển Giao Hàng</a></li>
+                        <li><a href="#">Chính Sách Bảo Mật Thông Tin</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <h4>Follow us</h4>
+                    <div class="social mb-3">
+                        <a href="#" target="_blank">
+                            <img src="images/product/ic-fb.png" alt="" border="0" />
+                        </a>
+                        <a href="#" target="_blank">
+                            <img src="images/product/ic-instagram.png" alt="" border="0" />
+                        </a>
+                        <a href="#" target="_blank">
+                            <img src="images/product/ic-youtube.png" alt="" border="0" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://online.gov.vn/HomePage/CustomWebsiteDisplay.aspx?DocId=34289">
+                            <img src="images/product/logo-bocongthuong.png" alt="" border="0" width="120px" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="copyright mb-0">© Copyright Fitfood.</p>
+    </div>
+</footer>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.bxslider.min.js"></script>
+<script src="js/jquery.cookie.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+
+</html>
