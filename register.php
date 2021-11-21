21cf5de960777fb3e0bc375fec0c415475251c21
leoPkm <thinhphatmai2001@gmail.com>
2021-11-21 12:46:14 +0700

    register v1

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 ?>
 
 
-
+<?php if(isset($_POST['name'])){ echo htmlentities($_POST['name']);} ?>
 
 <!doctype html>
 <html lang="en">
@@ -27,35 +27,35 @@
         <form  action="<?php echo $_SERVER["PHP_SELF"];?>"  name="registerForm" method="post" onsubmit="return registervalidation()">
           <div class="mb-3 mt-3">
             <label for="username">Họ và tên người dùng:</label>
-            <input type="text" class="form-control" id="username" placeholder="Nhập họ và tên..." name="username" >
+            <input type="text" class="form-control" id="username" placeholder="Nhập họ và tên..." name="username" value="<?php if(isset($_POST['username'])){ echo htmlentities($_POST['username']);} ?>">
           </div>
           <p class="small text-danger" id="namewrongmessage"></p>
 
 
           <div class="mb-3 mt-3">
             <label for="useraccount">Tài khoản:</label>
-            <input type="text" class="form-control" id="useraccount" placeholder="Nhập tài khoản..." name="useraccount" >
+            <input type="text" class="form-control" id="useraccount" placeholder="Nhập tài khoản..." name="useraccount" value="<?php if(isset($_POST['useraccount'])){ echo htmlentities($_POST['useraccount']);} ?>" >
           </div>
           <p class="small text-danger" id="accwrongmessage"></p>
           <div class="mb-3">
             <label for="pwd">Mật khẩu:</label>
-            <input type="password" class="form-control" id="pwd" placeholder="Nhập mật khẩu..." name="pswd" >
+            <input type="password" class="form-control" id="pwd" placeholder="Nhập mật khẩu..." name="pswd" value="<?php if(isset($_POST['pswd'])){ echo htmlentities($_POST['pswd']);} ?>" >
           </div>
           <p class="small text-danger" id="passwrongmessage"></p>
           <div class="mb-3">
             <label for="re_pwd">Xác nhận mật khẩu:</label>
-            <input type="password" class="form-control" id="re_pwd" placeholder="Nhập lại mật khẩu..." name="re_pswd" >
+            <input type="password" class="form-control" id="re_pwd" placeholder="Nhập lại mật khẩu..." name="re_pswd" value="<?php if(isset($_POST['re_pswd'])){ echo htmlentities($_POST['re_pswd']);} ?>" >
           </div>
           <p class="small text-danger" id="repasswrongmessage"></p>
 
           <div class="mb-3 mt-3">
             <label for="email">Thư điện tử:</label>
-            <input type="email" class="form-control" id="email" placeholder="Nhập Thư điện tử..." name="email" >
+            <input type="email" class="form-control" id="email" placeholder="Nhập Thư điện tử..." name="email" value="<?php if(isset($_POST['email'])){ echo htmlentities($_POST['email']);} ?>">
           </div>
 
           <div class="mb-3 mt-3">
             <label for="Phone_number">Sổ điện thoại:</label>
-            <input type="tel" class="form-control" id="phone_number" placeholder="Nhập số điện thoại" name="phone_number" >
+            <input type="tel" class="form-control" id="phone_number" placeholder="Nhập số điện thoại" name="phone_number" value="<?php if(isset($_POST['phone_number'])){ echo htmlentities($_POST['phone_number']);} ?>" >
           </div>
           <p class="small text-danger" id="phonewrongmessage"></p>
           <div class="clearfix">
