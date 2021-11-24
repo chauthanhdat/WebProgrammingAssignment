2a7d76077251bbb58080608176fe15f79e0e5d92
leoPkm <thinhphatmai2001@gmail.com>
2021-11-25 02:07:21 +0700

    logout ok

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
-include './inc/header.php'
+session_start();
+?>
+
+<?php
+
+    if(isset($_POST['Logout'])){
+        // remove cookie
+        if(isset($_COOKIE[session_name()])){
+            setcookie(session_name(),'',time() - 3600, '/');
+        }
+
+        // unset data in $_SESSION
+        $_SESSION[] = array();
+
+        // destroy the session
+        session_destroy();
+        unset($_POST['Logout']);
+        header('Location: index.php');
+        unset($_POST);
+    }
+
+
+include './inc/header.php';
 //ahfiashd jflasdj flasj alkdj sklfjal
 ?>
 
