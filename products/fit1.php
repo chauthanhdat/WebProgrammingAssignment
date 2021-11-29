3828dee4a692849e39a3bdc6bebd95e73d18e9fe
leoPkm <thinhphatmai2001@gmail.com>
2021-11-29 10:52:07 +0700

    da them tinh nang gio hang nhung chua du

--- products/fit1.php
+++ products/fit1.php
@@ -5,6 +5,81 @@
     include 'productheader.php';
 ?>
 
+<?php 
+
+//them vao gio hang
+if(isset($_POST['submit'])){
+    $dbservername = "localhost";
+    $dbusername = "root";
+    $dbname = "fitfooddb";
+    $loginsuccess = false;
+    $connection = mysqli_connect($dbservername, $dbusername, "", $dbname);
+    if (!$connection) {
+        echo "kết nối với csdl thất bại: " . mysqli_connect_error();
+        exit;
+    }
+    //kiem tra co gio hang chua
+    $ur_id=mysqli_real_escape_string($connection,$_SESSION['Id']);
+    $sql_query="SELECT `cart_id`,`cart_ur_id` from `cart` where  cart_ur_id={$_SESSION['Id']} AND cart_delivered=0";
+    $query_result = mysqli_query($connection, $sql_query);
+    $numcart = mysqli_num_rows($query_result);
+    $i=0;
+    if($numcart<=0){    // chưa có giỏ hàng
+        $sql_query = "INSERT INTO `cart`( `cart_ur_id`) VALUES ({$_SESSION['Id']});";
+        mysqli_query($connection, $sql_query);
+    }
+    //mysqli_real_escape_string
+      // có giỏ hàng hoac sau khi da tao xong gio hang lấy id cua gio hang
+    $da_giao=0; //0 = don chua giao - 1 = da giao
+    //$da_giao=mysqli_real_escape_string($connection,$da_giao);
+    $sql_query="SELECT `cart_id`,`cart_ur_id` from `cart` where  cart_ur_id={$_SESSION['Id']} AND cart_delivered={$da_giao}";
+    //echo "<h1><br><br><br>$sql_query</h1>";
+    $query_result = mysqli_query($connection, $sql_query);
+    $numcart = mysqli_num_rows($query_result);
+    $cart_info=[];
+    while($row=mysqli_fetch_assoc($query_result)){
+        if($i==($numcart-1)){
+            $cart_info['cart_id']=$row['cart_id'];
+            //echo "<h1><br><br><br>{$cart_info['cart_id']}</h1>";
+        }
+    }
+
+    //     //lay thong tin ve food roi them vao;
+        $food_name="GOI FIT 1";
+        $food_name = mysqli_real_escape_string($connection, $food_name);
+        $sql_query_select="SELECT `food_id`,`food_name`,`food_price` from `food` where  `food_name`='$food_name';";
+        
+    //     //mysqli_free_result($query_result);
+        $query_result = mysqli_query($connection,$sql_query_select);
+        $food_info=[];
+        if( mysqli_num_rows($query_result)>0){
+            while($row=mysqli_fetch_assoc($query_result)){
+                $food_info['food_id']=$row['food_id'];
+                $food_info['food_name']=$row['food_name'];
+                $food_info['food_price']=$row['food_price'];
+            }
+       }
+    //   echo "<h1><br><br><br><br>{$food_info['food_id']}<br>{$food_info['food_name']}<br>{$food_info['food_price']}<br></h1>";
+    //     // lấy id gio hang rồi thêm vào
+        $price= $food_info['food_price'];
+        $food_id=$food_info['food_id'];
+        $cart_id=$cart_info['cart_id'];
+        $food_name=$food_info['food_name'];
+        $sql_query_select="SELECT `fd_id`, `crt_id` from `cart_it` where `fd_id`=$food_id and `crt_id`=$cart_id;";
+        $query_result = mysqli_query($connection,$sql_query_select);
+
+        if(mysqli_num_rows($query_result)<=0){
+            $sql_query_insert ="INSERT INTO `cart_it`(`fd_id`, `crt_id`, `fd_name`, `num`, `it_tool`) VALUES ($food_id,$cart_id,'$food_name',1,$price);";
+            //echo "<h1><br><br><br><br>{$sql_query_insert}</h1>";
+            $query_result = mysqli_query($connection, $sql_query_insert);
+        }
+
+
+}
+
+?>
+
+
 <div class="top-inner">
     <div class="container">
         <div class="row mb-5">
@@ -25,8 +100,9 @@
                     <p>- Kèm tinh bột phức, ít đường, đảm bảo ko bột ngọt, nhiều rau củ và chất đạm</p>
                     <p> <i> * Thích hợp cho dân văn phòng cần ít năng lượng, dành thời gian dùng bữa tối cùng gia đình</i></p>
                 </div>
-                <form action="" method="post" id='add-to-cart' name="add-to-cart"></form>
-                <button type="submit" class="btn btn-danger" form="add-to-cart" value="Submit">Thêm vào giỏ hàng</button>
+                <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" name='add-to-cart' name="add-to-cart" onsubmit="return check_login()">
+                <button type="submit" class="btn btn-danger" name="submit" >Thêm vào giỏ hàng</button>
+                </form>
             </div>
         </div>
     </div>
